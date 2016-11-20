e03dba565d22314c259240487da1fad02c5e5ad3
Rahul Ranjan <ranjan.rahul706@gmail.com>
2016-11-20 20:32:08 +0530

    first commit

---
+++ app/src/main/java/com/thenewboston/contactmanagementusingsqlitedatabase/databaseHelper.java
@@ -0,0 +1,28 @@
+package com.thenewboston.contactmanagementusingsqlitedatabase;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Rahul on 20-11-2016.
+ */
+
+public class databaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "contact.db";
+    private static final String TABLE_NAME = "contacts";
+    public databaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
