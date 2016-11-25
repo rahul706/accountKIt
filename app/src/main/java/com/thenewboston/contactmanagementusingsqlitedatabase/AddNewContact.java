f16866cab8288c669923c008e92ce5e080cb0f37
Rahul Ranjan <ranjan.rahul706@gmail.com>
2016-11-25 20:57:42 +0530

    add new contact activity

---
+++ app/src/main/java/com/thenewboston/contactmanagementusingsqlitedatabase/AddNewContact.java
@@ -0,0 +1,22 @@
+package com.thenewboston.contactmanagementusingsqlitedatabase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddNewContact extends AppCompatActivity {
+
+    EditText name,mobile_number,email;
+    Button save;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_contact);
+
+        name = (EditText) findViewById(R.id.etName);
+        mobile_number = (EditText) findViewById(R.id.enMobileNumber);
+        email= (EditText) findViewById(R.id.etEmail);
+        save = (Button) findViewById(R.id.btnSave);
+    }
+}
